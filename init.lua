9acdecfefa0740bcc50ea1ecd2fe4e06056adaf1
k33m0l <199402465+k33m0l@users.noreply.github.com>
2025-06-17 09:37:11 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -588,6 +588,8 @@ require('lazy').setup({
         clangd = {},
         -- gopls = {},
         -- pyright = {},
+        
+        -- Needs cargo
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
